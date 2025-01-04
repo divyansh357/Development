32216dd907484b3e84b7e830dfb40ed073a920e8
divyansh357 <divyanshkumargupta532@gmail.com>
2025-01-05 00:07:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -781,3 +781,211 @@
 // {
 //     console.log("Element not present in the array.");
 // }
+
+//  Js-4
+//Loops
+
+//1. for loop
+
+// for(let i=1;i<=5;i++)
+// {
+//     console.log(i);
+// }
+
+// for(let i=10;i>=1;i=i-3)
+// {
+//     console.log(i);
+// }
+
+//Odd numbers from 1 to 15
+// for(let i=1;i<=15;i+=2)
+// {
+//     console.log(i);
+// }
+
+// for(let i=15;i>=1;i-=2)
+// {
+//     console.log(i);
+// }
+
+// Even numbers from 2 to 10
+
+// for(let i=2;i<=10;i+=2)
+// {
+//     console.log(i);
+// }    
+
+// for(let i=10;i>=2;i-=2)
+// {
+//     console.log(i);
+// }
+
+// Infinite loop
+
+// for(let i=1; ; i++)
+// {
+//     console.log(i);
+// }
+
+// Multiplication table
+
+// for(let i=5;i<=50;i+=5)
+// {
+//     console.log(i);
+// }
+
+// let num = prompt("Enter a number:");
+// num = parseInt(num); // converting integer to number 
+// for(let i=num;i<=num*10;i+=num)
+// {
+//     console.log(i);
+// }
+
+// Nested for loop
+
+// for(let i=1;i<=3;i++)
+// {
+//     console.log(`Outer loop:${i}`);
+//     for(let j=1;j<=3;j++)
+//     {
+//         console.log(j);
+//     }
+// }
+
+// 2. while loop
+
+// let i =1;
+// while(i<=5)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+// // backwards
+
+// let j = 5;
+// while(j>=1)
+// {
+//     console.log(j);
+//     j--;
+// }
+
+// let k=0;
+// while(k<=20)
+// {
+//     console.log(k);
+//     k+=2;
+// }
+
+// Favorite Movie
+
+// const movie = "The Dark Knight";
+// let guess = prompt("Guess my favorite movie:");
+
+// while((guess!="The Dark Knight")&&(guess!="quit"))
+// {
+//     guess = prompt("Wrong guess. Please try again:");
+// }
+
+// if(guess=="The Dark Knight")
+// {
+//     console.log("You guessed it right!");
+// }
+// else
+// {
+//     console.log("You quit the game.");
+// }
+
+//break keyword
+
+// let i =1;
+// while(i<=5)
+// {
+    
+//     if(i==3)
+//     {
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// Customize favorite movie with break
+
+// const favMovie = "Avengers";
+// let guess = prompt("Guess my favorite movie:");
+// while(guess!="Avengers")
+// {
+//     if(guess=="quit")
+//         {
+//             console.log("You quit the game.");
+//             break;
+//         }
+//     guess = prompt("Wrong guess. Please try again or you can quit by typing quit:");
+// }
+// if(guess=="Avengers")
+// {
+//     console.log("You guessed it right!");
+// }
+
+// Loops with arrays
+
+// let fruits = ["Apple","Banana","Mango","Pineapple","Orange"];
+// fruits.push("Grapes");
+// for(let i=0;i<fruits.length;i++)
+// {
+//     console.log(i,fruits[i]);
+// }
+
+// for(let i = fruits.length-1;i>=0;i--)
+// {
+//     console.log(i,fruits[i]);
+// }
+
+// Loops with nested arrays
+
+// let heros = [["ironman","spiderman","thor"],
+//                 ["superman","wonder woman","flash"]];
+// for(let i=0;i<heros.length;i++)
+// {
+//     console.log(i,heros[i]);
+//     for(let j=0;j<heros[i].length;j++)
+//     {
+//         console.log(`j=${j} ${heros[i][j]}`);
+//     }
+// }
+
+// let student = [["Divyansh",20,],["Rahul",21],["Rohit",22]];
+// for(let i=0;i<student.length;i++)
+// {
+//     console.log("Info of student:",i+1);
+//     for(let j=0;j<student[i].length;j++)
+//     {
+//         console.log(student[i][j]);
+//     }
+// }
+
+// for of loop
+
+// let fruits = ["Apple","Banana","Mango","Pineapple","Orange"];
+// for(let fruit of fruits)
+// {
+//     console.log(fruit);
+// }
+
+// for(char of "apna college")
+// {
+//     console.log(char);
+// }
+
+// Nested for of loop
+
+let heroes = [["ironman","spiderman","thor"],["superman","wonder woman","flash"]];
+for(list of heroes)
+{
+    console.log(list);
+    for(hero of list)
+    {
+        console.log(hero);
+    }
+}
