acb1262cac5e4fbe87cb7014da43fd43e0594576
Divyansh Kumar Gupta <divyanshkumargupta532@gmail.com>
2025-03-06 16:50:57 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,74 @@
+// Js-9
+// making changes in h1 tag
+// console.dir(document);
+// document.all[8].innerText = "Peter Parker";
+
+// Selecting Elements 
+
+// getElementById
+
+// let imgObj= document.getElementById("mainImg");
+// console.dir(imgObj);
+// console.log(imgObj.src);
+// console.log(imgObj.tagName);
+// console.log(imgObj.id);
+
+// // making changes
+// imgObj.src = "assets/creation_1.png";
+// imgObj.src = "assets/spiderman_img.png";
+
+// Paragraph Object
+// let parObj = document.getElementById("description");
+// console.dir(parObj);
+
+// Wrong Id
+// console.dir(document.getElementById("aghfg"));
+
+//getElementByClassName 
+
+//let smallImages = document.getElementsByClassName("oldImg")
+// for(let i=0;i<smallImages.length;i++)
+// {
+//     console.dir(smallImages[i]);
+// }
+// console.log(document.getElementsByClassName("oldImg")[0].src);
+
+// for(let i=0;i<smallImages.length;i++)
+// {
+//     smallImages[i].src = "assets/spiderman_img.png";
+//     console.log(`Value of image no. ${i} is changed.`);
+    
+// }
+
+// Empty Collection
+//console.log(document.getElementsByClassName("abcd")); 
+
+// getElementByTagName
+
+// console.dir(document.getElementsByTagName("p"));
+
+// document.getElementsByTagName("p")[1].innerText = "abc";
+
+// Empty Collection
+//console.dir(document.getElementsByTagName("span"));
+
+// Query Selectors
+
+// console.dir(document.querySelector("h1"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".oldImg"));
+
+// Nesting
+// Getting anchor tag inside div
+// console.dir(document.querySelector("div a"));
+
+// // Getting all values 
+
+// console.dir(document.querySelectorAll("div a"));
+// console.dir(document.querySelectorAll("p"));
+
+// Setting Content in Objects
+
+
+
+
